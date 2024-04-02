d2a2c00b220b6fb9505b9173d279a89e02867a77
SiddharthPant <spsiddharthpant@gmail.com>
2024-04-02 09:31:40 +0530

    Add comment for handling validation aliases

--- server/models.py
+++ server/models.py
@@ -88,6 +88,22 @@ def validate_timestamp(v, handler):
 Float = Annotated[float, WrapValidator(validate_timestamp)]
 
 
+# Unfortunately there is a bug currently in versions of SQLModel > 0.0.13 that prevents
+# alias parameters from working. We can't go back as 0.0.13 will need to revert back
+# to pydantic v1 syntax which will cause too many breaking changes.
+# One solution for workaround was to manually override 2 resources FieldInfo and Field
+# but that again requires some time to get right.
+# Here are more details: https://github.com/tiangolo/sqlmodel/pull/774
+# class RecordIn(RecordBase):
+#     service_date: datetime = Field(validation_alias="service date")
+#     submitted_procedure: str = Field(validation_alias="submitted procedure")
+#     group_plan: str = Field(validation_alias="Plan/Group #")
+#     subscriber_id: str = Field(validation_alias="Subscriber#")
+#     provider_npi: str = Field(validation_alias="Provider NPI")
+#     provider_fees: Float = Field(validation_alias="provider fees")
+#     allowed_fees: Float = Field(validation_alias="Allowed fees")
+#     member_coinsurance: Float = Field(validation_alias="member coinsurance")
+#     member_copay: Float = Field(validation_alias="member copay")
 class RecordIn(RecordBase):
     provider_fees: Float
     allowed_fees: Float
