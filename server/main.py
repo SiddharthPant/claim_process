e474778d9b28baa2303099eee17407cef6a8c6dc
SiddharthPant <spsiddharthpant@gmail.com>
2024-04-02 05:54:19 +0530

    Improve server setup with custom unique IDs, routes and CORS

--- server/main.py
+++ server/main.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.routing import APIRoute
 
-app = FastAPI()
+from .api.main import api_router
+from .core.config import settings
 
 
-@app.get("/ping")
-async def pong():
-    return {"ping": "pong!"}
+def custom_generate_unique_id(route: APIRoute) -> str:
+    return f"{route.tags[0]}-{route.name}"
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    generate_unique_id_function=custom_generate_unique_id,
+)
+
+
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS
+        ],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
